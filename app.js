c6a436057669b4210884e4e11b6b8202d3527d1b
dylan-av42 <dylan.piersanti@av42.com>
2023-06-29 13:10:17 +0200

    init module

--- app.js
+++ app.js
@@ -2,4 +2,33 @@ const config = {
     version: 'v0.0.1'
 }
 
+function data() {
+    return {
+        url: window.location.href,
+        classes: {
+
+        },
+        getCurrentCharte,
+        setCharte,
+        setConditionalCollectionsItems
+    }
+}
+
+function setCharte(prefix) {
+
+}
+
+function setConditionalCollectionsItems(_class, dataAttribute, prefix) {
+
+}
+
+function getCurrentCharte() {
+    switch (true) {
+        default:
+            this.setCharte('oa')
+            this.classes.logo = 'https://assets-global.website-files.com/61f94b48d90359310ec28169/632abf38272edc1649ee5386_Logo_MSBA.svg'
+        break;
+    }
+}
+
 console.log(`MSBA 2023 - ${config.version} - initialisée avec succès ! 👌`)
